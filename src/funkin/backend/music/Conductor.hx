3d7509c430862d828e3021f49b76301171b2930a
Fyrid19 <jkeonde@gmail.com>
2024-09-25 22:08:33 -0400

    Finished the conductor (i think)

--- src/funkin/backend/music/Conductor.hx
+++ src/funkin/backend/music/Conductor.hx
@@ -9,17 +9,19 @@ typedef BPMChangeEvent = {
 }
 
 /**
- * the entire backend of the game, VERY important
+ * The music backend for the game.
  */
-@:nullSafety()
-class Conductor {
+class Conductor extends FlxBasic {
     private var _initialized:Bool = false;
     // variables
-    public var bpm(get, set):Float = 100;
+    public var bpm:Float = 100;
     public var forceBPM:Bool = false;
     
     public var offset:Float = 0;
     public var songPosition:Float = 0;
+
+    public var curSong:FlxSound = new FlxSound();
+    public var autoPlay:Bool = false;
 
     public var curStep:Int = 0;
     public var curBeat:Int = 0;
@@ -48,23 +50,11 @@ class Conductor {
     public var onSongStart:FlxSignal = new FlxSignal();
     public var onSongEnd:FlxSignal = new FlxSignal();
 
-    // private vars
-    private var _bpm:Float = 100;
-
     // static instance
     public static var instance(get, set):Conductor;
     private static var _instance:Conductor;
 
     // getters n setters
-    function get_bpm() {
-        return _bpm;
-    }
-
-    function set_bpm(newBpm:Float) {
-        resetCrochet(newBpm);
-        return _bpm = newBpm;
-    }
-
     static function get_instance() {
         if (_instance != null) {
             return _instance;
@@ -82,14 +72,66 @@ class Conductor {
         return _instance = newConductor;
     }
 
-    // initialize the conductor
-    public function new() {
-        instance = new Conductor();
-        _initialized = true;
+    public function new(?song:FlxSound, ?autoPlay:Bool, ?bpm:Float = 100, ?timeSignature:Array<Int> = [4, 4]) {
+        super();
+        
+        this.curSong = song;
+        this.autoPlay = autoPlay;
+        this.bpm = bpm;
+        this.beatsPerMeasure = timeSignature[0];
+        this.stepsPerBeat = timeSignature[1];
+
+        if (autoPlay) {
+            if (song != null) {
+                song.play();
+            }
+        }
+
+        if (curSong != null) {
+            if (curSong.playing) {
+                songPosition = curSong.time;
+            }
+        } else {
+            if (FlxG.sound.music.playing) {
+                songPosition = FlxG.sound.music.time;
+            }
+        }
+
+        onStepHit.removeAll();
+        onBeatHit.removeAll();
+        onMeasureHit.removeAll();
     }
 
-    public function update(elapsed:Float) {
-        
+    override public function update(elapsed:Float) {
+        super.update(elapsed);
+
+        var oldStep:Int = curStep;
+
+        updateStep();
+        updateBeat();
+        updateMeasure();
+
+        if (oldStep < curStep && curStep >= 0)
+            stepHit();
+    }
+    
+    private function updateMeasure() {
+        curMeasure = Math.floor(curBeat / beatsPerMeasure);
+    }
+
+    private function updateBeat() {
+        curBeat = Math.floor(curStep / stepsPerBeat);
+    }
+
+    private function updateStep() {
+        var bpmChange:BPMChangeEvent = {};
+
+        for (i in 0...bpmMap.length) {
+            if (songPosition == bpmMap[i].songTime)
+                bpmChange = bpmMap[i];
+        }
+
+        curStep = bpmChange.stepTime + Math.floor((songPosition - bpmChange.songTime) / stepCrochet);
     }
 
     public function stepHit() {
@@ -99,26 +141,29 @@ class Conductor {
     }
 
     public function beatHit() {
-
+        if (curStep % beatsPerMeasure == 0)
+            measureHit();
+        onBeatHit.dispatch();
     }
     
     public function measureHit() {
-        
+        onMeasureHit.dispatch();
     }
 
     // songs dont actually   exist yet
     // just keeping this here for future use
-    public static function mapBPMChanges(song:Void) {
+    public function mapBPMChanges(song:Void) {
         bpmMap = [];
     }
 
-    public static function resetCrochet(bpm:Float) {
+    public function resetCrochet(bpm:Float) {
         crochet = (60 / bpm) * 1000;
         stepCrochet = crochet / stepsPerBeat;
         measureCrochet = crochet * beatsPerMeasure;
     }
 
-    public static function changeBPM(newBPM:Float) {
-        bpm = _bpm;
+    public function changeBPM(newBPM:Float) {
+        bpm = newBPM;
+        resetCrochet();
     }
 }
---
+++ src/funkin/import.hx
@@ -0,0 +1,11 @@
+package funkin;
+
+import flixel.FlxG;
+import flixel.FlxBasic;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flixel.sound.FlxSound;
+import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
+
+import funkin.backend.music.Conductor;
